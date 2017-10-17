2955e54a772f64ab93a730758eae532c2f645970
setom <matthew.y.seto@gmail.com>
2017-10-17 14:38:15 -0700

    Added Interactive Map to fit with requirements

--- assets/js/modalCtrl.js
+++ assets/js/modalCtrl.js
@@ -24,6 +24,9 @@ window.onclick = function(event) {
 //the event handler for the buttonclick to open the modal
 function openModal(id){
     console.log("clicked modal open button " + id);
+    if (id=="embeddedMap"){
+        document.getElementById("modalIFrame").innerHTML = '<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d21566.10478554512!2d-121.78214228630621!3d47.4945282521766!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1508275934130" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>"';
+    }
     document.getElementById("modalTitle").innerHTML = id;
     modal.style.display = "block";
 }
